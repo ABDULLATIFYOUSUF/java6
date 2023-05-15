b292925c0c7d4e581cd090008e27021aa49ebc1e
latif <latifyousuf513@gmail.com>
2023-05-15 18:05:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// CHP # 43-48
+
+// Q # 1
+// function msg(){
+//     alert("Hello")
+// }
+
+// // Q # 2
+// function msg1(){
+//     alert("Thanks for purchase mobile from us")
+// }
+
+// // Q # 3
+
+
+// function deleteLi(e){
+//     e.parentNode.remove(e)
+    
+// }
+
+// // Q # 4
+// function over(){
+//     document.getElementById("before").src = "./images/2.jpg"
+// }
+
+// function out(){
+//     document.getElementById("before").src = "./images/1.jfif"
+// }
